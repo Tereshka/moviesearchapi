4377bef5da4826b94a771a3f335e88791cc289c3
Elena <tereshka07@gmail.com>
2018-05-30 12:25:55 +0900

    add files

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+
+var request = require("request");
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3001; 
+
+app.get("/", function(req, res){
+	res.render("search");
+});
+
+app.get("/results", function(req, res){
+	var search = req.query.search;
+	var url = "http://www.omdbapi.com/?apikey=b08e95e3&s=" + search;
+	request(url, function(err, response, body){
+		if(!err && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.render("results", {data});
+		}
+	})
+});
+
+app.get("/movie/:id", function(req, res){
+	var url = "http://www.omdbapi.com/?apikey=b08e95e3&i=" + req.params.id;
+	request(url, function(err, response, body){
+		if(!err && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.render("movie", {data});
+		}
+	})
+});
+
+app.listen(PORT, function(){
+	console.log("server starts");
+});
